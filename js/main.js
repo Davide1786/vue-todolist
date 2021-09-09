a7e768d9bce138e08ed711133f3759910a8a0754
Davide <dadavidenapolitano@gmail.com>
2021-09-09 17:31:08 +0200

    Esercizio + Bonus

--- js/main.js
+++ js/main.js
@@ -1,16 +1,63 @@
 Vue.config.devtools = true;
 
+// const app = new Vue (
+//    {
+//       el: '#root',
+//       data: {
+//          add: '',
+//          listaPromemoria: [
+//             'Accendere il computer',
+//             'Inserire la password',
+//             'Aprire l\'ediotr di codice',
+//             'Aprire Google',
+//             'Iniziare a scrivere codice per svolgere l\'esercizio'
+//          ]
+//       },
+//       methods: {
+//          addPromemoria() {
+//             if(this.add == ''){
+//                alert('Inserisci un promemoria');
+//             }
+//             else {
+//                this.listaPromemoria.push(this.add);
+//                this.add = '';
+//             }
+//          },
+//          removeProme(index) {
+//             this.listaPromemoria.splice(index, 1);
+//          },
+//          onEnter() {
+//             this.listaPromemoria.push(this.add);
+//             this.add = '';
+//          }
+//       }
+//    }
+// )
+
+// =========================================================== versione con array di oggetti
+
 const app = new Vue (
    {
       el: '#root',
       data: {
          add: '',
-         listaPromemoria: [
-            'Accendere il computer',
-            'Inserire la password',
-            'Aprire l\'ediotr di codice',
-            'Aprire Google',
-            'Iniziare a scrivere codice per svolgere l\'esercizio'
+         listaPromemoria: [ 
+            {
+               titolo: 'Pulire la casa',
+               done: true,
+            },
+            {
+               titolo: 'Aprire i balconi',
+               done: false,
+            },
+            {
+               titolo: 'Cambiare le lenzuola',
+               done: false,
+            },
+            {
+               titolo: 'Fare la spesa',
+               done: true,
+            },
          ]
       },
       methods: {
@@ -19,7 +66,7 @@ const app = new Vue (
                alert('Inserisci un promemoria');
             }
             else {
-               this.listaPromemoria.push(this.add);
+               this.listaPromemoria.push({titolo: this.add, done: false});
                this.add = '';
             }
          },
@@ -27,9 +74,14 @@ const app = new Vue (
             this.listaPromemoria.splice(index, 1);
          },
          onEnter() {
-            this.listaPromemoria.push(this.add);
+            this.listaPromemoria.push({titolo: this.add, done: false});
             this.add = '';
+         },
+         cambioColore(promemoria){
+           promemoria.done = !promemoria.done;
+      
          }
       }
    }
 )
+
